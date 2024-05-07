4447502b15fb0c5471e9a2f0526e5e25c0844281
ygdehftygduhs <gs1airheart@hotmail.com>
2024-05-07 21:11:44 +0700

    hello

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+const stoogeSort = (arr, i = 0, j = arr.length - 1) => {
+  if (arr[i] > arr[j]) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  if (i + 1 >= j) {
+    return arr;
+  }
+  const t = Math.floor((j - i + 1) / 3);
+  stoogeSort(arr, i, j - t);
+  stoogeSort(arr, i + t, j);
+  stoogeSort(arr, i, j - t);
+  return arr;
+};
