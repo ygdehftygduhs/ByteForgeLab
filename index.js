87a2cf931ab0e24e4ba3c52cd6b0036bf974b042
ygdehftygduhs <gs1airheart@hotmail.com>
2024-05-08 14:19:22 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const stoogeSort = (arr, i = 0, j = arr.length - 1) => {
-  if (arr[i] > arr[j]) {
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
   }
-  if (i + 1 >= j) {
-    return arr;
-  }
-  const t = Math.floor((j - i + 1) / 3);
-  stoogeSort(arr, i, j - t);
-  stoogeSort(arr, i + t, j);
-  stoogeSort(arr, i, j - t);
-  return arr;
+  return -1;
 };
