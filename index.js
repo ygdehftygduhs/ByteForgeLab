65d789c097f197afd2ee56d6452ad7219af0dce3
ygdehftygduhs <gs1airheart@hotmail.com>
2024-05-09 12:11:36 +0700

    feat project commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const linearSearch = (arr, target) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
-    }
-  }
-  return -1;
-};
+function minDepth(root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  let min = Infinity;
+  if (root.left) min = Math.min(min, minDepth(root.left));
+  if (root.right) min = Math.min(min, minDepth(root.right));
+  return min + 1;
+}
