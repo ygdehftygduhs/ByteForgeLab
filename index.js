f9caa810387114e1d38329c368c3746a3adcb786
ygdehftygduhs <gs1airheart@hotmail.com>
2024-05-11 12:30:32 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-function letterCombinations(digits) {
-  if (digits.length === 0) return [];
-  const map = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
-  const result = [];
-  backtrack("", 0);
-  return result;
-  function backtrack(current, index) {
-    if (current.length === digits.length) {
-      result.push(current);
-      return;
-    }
-    const letters = map[digits[index]];
-    for (const letter of letters) {
-      backtrack(current + letter, index + 1);
+function ladderLength(beginWord, endWord, wordList) {
+  const wordSet = new Set(wordList);
+  if (!wordSet.has(endWord)) return 0;
+  let count = 0;
+  const queue = [beginWord];
+  while (queue.length) {
+    const size = queue.length;
+    count++;
+    for (let i = 0; i < size; i++) {
+      const current = queue.shift();
+      if (current === endWord) return count;
+      for (let j = 0; j < current.length; j++) {
+        for (let k = 97; k <= 122; k++) {
+          const newWord =
+            current.slice(0, j) + String.fromCharCode(k) + current.slice(j + 1);
+          if (wordSet.has(newWord)) {
+            queue.push(newWord);
+            wordSet.delete(newWord);
+          }
+        }
+      }
     }
   }
+  return 0;
 }
