755b0d0d8d3c424447c6940898f7ae9e6b9875a4
ygdehftygduhs <gs1airheart@hotmail.com>
2024-05-13 07:48:51 +0700

    test task

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-function ladderLength(beginWord, endWord, wordList) {
-  const wordSet = new Set(wordList);
-  if (!wordSet.has(endWord)) return 0;
-  let count = 0;
-  const queue = [beginWord];
-  while (queue.length) {
-    const size = queue.length;
-    count++;
-    for (let i = 0; i < size; i++) {
-      const current = queue.shift();
-      if (current === endWord) return count;
-      for (let j = 0; j < current.length; j++) {
-        for (let k = 97; k <= 122; k++) {
-          const newWord =
-            current.slice(0, j) + String.fromCharCode(k) + current.slice(j + 1);
-          if (wordSet.has(newWord)) {
-            queue.push(newWord);
-            wordSet.delete(newWord);
-          }
-        }
-      }
-    }
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
   }
-  return 0;
+  return currMax;
 }
